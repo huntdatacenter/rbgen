1429a6c81781924fb87f589e37146cbd53fd7a0f
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk>
2015-11-05 21:26:43 +0000

    genfile: add a byte_t typedef (which is uint8_t and use it for buffers in bgen

--- genfile/include/genfile/zlib.hpp
+++ genfile/include/genfile/zlib.hpp
@@ -21,21 +21,21 @@ namespace genfile {
 	//
 	// If offset is nonzero, compressed data will be written starting at position [offset].
 	// The first [offset] bytes will be untouched.
-	void zlib_compress( char const* buffer, char const* const end, std::vector< char >* dest, std::size_t const offset = 0 ) ;
+	void zlib_compress( unsigned char const* buffer, unsigned char const* const end, std::vector< unsigned char >* dest, std::size_t const offset = 0 ) ;
 
 	// Compress the given data into the given destination buffer.  The destination will be resized
 	// to fit the compressed data.  (Since the capacity of dest may be larger than its size,
 	// to save memory you may need to copy the contents of dest elsewhere after calling
 	// this function).
 	template< typename T >
-	void zlib_compress( std::vector< T > const& source, std::vector< char >* dest ) {
-		char const* begin = reinterpret_cast< char const* >( &source[0] ) ;
-		char const* const end = reinterpret_cast< char const* >( &source[0] + source.size() ) ;
+	void zlib_compress( std::vector< T > const& source, std::vector< unsigned char >* dest ) {
+		unsigned char const* begin = reinterpret_cast< unsigned char const* >( &source[0] ) ;
+		unsigned char const* const end = reinterpret_cast< unsigned char const* >( &source[0] + source.size() ) ;
 		return zlib_compress( begin, end, dest ) ;
 	}
 
 	template< typename T >
-	void zlib_uncompress( char const* begin, char const* const end, std::vector< T >* dest ) {
+	void zlib_uncompress( unsigned char const* begin, unsigned char const* const end, std::vector< T >* dest ) {
 		uLongf const source_size = ( end - begin ) ;
 		uLongf dest_size = dest->size() * sizeof( T ) ;
 		int result = uncompress(
@@ -53,9 +53,9 @@ namespace genfile {
 	// The destination must be large enough to fit the uncompressed data,
 	// and it will be resized to exactly fit the uncompressed data.
 	template< typename T >
-	void zlib_uncompress( std::vector< char > const& source, std::vector< T >* dest ) {
-		char const* begin = &source[0] ;
-		char const* const end = &source[0] + source.size() ;
+	void zlib_uncompress( std::vector< unsigned char > const& source, std::vector< T >* dest ) {
+		unsigned char const* begin = &source[0] ;
+		unsigned char const* const end = &source[0] + source.size() ;
 		zlib_uncompress( begin, end, dest ) ;
 	}
 }
