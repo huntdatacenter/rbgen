17a4874f1a772de40e94a25fc7cc5aadcc497ca5
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk>
2017-05-16 12:04:49 +0000

    fix R/package/test.sh to refer to existing indexed file

--- R/package/test.sh
+++ R/package/test.sh
@@ -2,7 +2,7 @@
 R CMD INSTALL ./
 R --vanilla << HERE_DOC
 library( rbgen )
-D = giveMeMyData( "../example/example.8bits.bgen", chromosome = '01', start = 0, end = 100000 )
+D = giveMeMyData( "example/example.16bits.bgen", chromosome = '01', start = 0, end = 100000 )
 str( D )
 head( D\$variants )
 D\$data[1:2,1:10,1]
