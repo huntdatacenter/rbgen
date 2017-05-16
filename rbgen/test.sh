08ce0454bb0b95b1517cfe76b4d5499303c71a16
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk>
2017-05-16 11:47:42 +0000

    Added test.sh as simple test of rbgen package

---
+++ rbgen/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+R CMD INSTALL ./
+R --vanilla << HERE_DOC
+library( rbgen )
+D = giveMeMyData( "../example/example.8bits.bgen", chromosome = '01', start = 0, end = 100000 )
+str( D )
+head( D\$variants )
+D\$data[1:2,1:10,1]
+HERE_DOC
