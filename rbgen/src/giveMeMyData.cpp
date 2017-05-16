f2ba3bd6f986c13df558ab274334044f119d98d5
gavin.band@well.ox.ac.uk <gavin.band@well.ox.ac.uk>
2017-05-16 10:34:56 +0000

    rbgen::giveMeMyData(): add samples and variant names to result data

--- rbgen/src/giveMeMyData.cpp
+++ rbgen/src/giveMeMyData.cpp
@@ -1,7 +1,9 @@
 #include "genfile/bgen/View.hpp"
 #include "genfile/bgen/IndexQuery.hpp"
 #include <Rcpp.h>
-#if 1
+
+#define DEBUG 0
+
 namespace {
 	struct DataFiller {
 		typedef Rcpp::IntegerVector IntegerVector ;
@@ -25,12 +27,24 @@ namespace {
 		// Called once allowing us to set storage.
 		void initialise( std::size_t number_of_samples, std::size_t number_of_alleles ) {
 			// Nothing to do, but check storage is big enough.
-			assert( m_number_of_alleles == 2 ) ;
-			assert( m_data_dimension[0] > m_variant_i ) ;
-			assert( m_data_dimension[1] == number_of_samples ) ;
-			assert( m_data_dimension[2] == 3 ) ;
-			assert( m_ploidy_dimension[0] > m_variant_i ) ;
-			assert( m_ploidy_dimension[1] == number_of_samples ) ;
+			if(!(
+				(number_of_alleles == 2)
+				&& ( m_data_dimension[0] > m_variant_i )
+				&& ( m_data_dimension[1] == number_of_samples )
+				&& ( m_data_dimension[2] == 3 )
+				&& ( m_ploidy_dimension[0] > m_variant_i )
+				&& ( m_ploidy_dimension[1] == number_of_samples )
+			)) {
+#if DEBUG
+				std::cerr << "initialise(): invalid constraint.\n" ;
+				std::cerr << "Data dimension:\n" << m_data_dimension[0] << ", " << m_data_dimension[1] << ", " << m_data_dimension[2] << "\n" ;
+				std::cerr << "Ploidy dimension:\n" << m_ploidy_dimension[0] << ", " << m_ploidy_dimension[1] << "\n" ;
+				std::cerr << "Number of samples: " << number_of_samples << "\n" ;
+				std::cerr << "Number of alleles: " << number_of_alleles << "\n" ;
+				std::cerr << "Variant: " << m_variant_i << "\n" ;
+#endif
+				throw genfile::bgen::BGenError() ;
+			}
 		}
 	
 		// If present with this signature, called once after initialise()
@@ -57,20 +71,29 @@ namespace {
 			genfile::OrderType order_type,
 			genfile::ValueType value_type
 		) {
-			assert( value_type == genfile::eProbability ) ;
+			if( value_type != genfile::eProbability ) {
+				throw genfile::bgen::BGenError() ;
+			}
+			// assert( value_type == genfile::eProbability ) ;
 			m_order_type = order_type ;
 			int flatIndex = m_variant_i + m_sample_i * m_ploidy_dimension[0] ;
-			m_ploidy[ flatIndex ] = ploidy ;
+			(*m_ploidy)[ flatIndex ] = ploidy ;
 		}
 
 		void set_value( uint32_t entry_i, double value ) {
 			int flatIndex = m_variant_i + m_sample_i * m_data_dimension[0] + entry_i * m_data_dimension[0] * m_data_dimension[1] ;
-			m_data[ flatIndex ] = value ;
+#if DEBUG
+			std::cerr << "Setting data for index " << m_variant_i << ", " << m_sample_i << ", " << entry_i << ": index " << flatIndex << "...\n" << std::flush ;
+#endif
+			(*m_data)[ flatIndex ] = value ;
 		}
 
 		void set_value( uint32_t entry_i, genfile::MissingValue value ) {
 			int flatIndex = m_variant_i + m_sample_i * m_data_dimension[0] + entry_i * m_data_dimension[0] * m_data_dimension[1] ;
-			m_data[ flatIndex ] = NA_REAL ;
+#if DEBUG
+			std::cerr << "Setting data for index " << m_variant_i << ", " << m_sample_i << ", " << entry_i << ": index " << flatIndex << "...\n" << std::flush ;
+#endif
+			(*m_data)[ flatIndex ] = NA_REAL ;
 		}
 
 		void finalise() {
@@ -86,11 +109,27 @@ namespace {
 		std::size_t const m_variant_i ;
 		std::size_t m_sample_i ;
 	
-		std::size_t m_number_of_alleles ;
 		genfile::OrderType m_order_type ;
 	} ;
+
+	struct set_sample_names {
+		set_sample_names( Rcpp::StringVector* result ):
+			m_result( result ),
+			m_index(0)
+		{
+			assert( result != 0 ) ;
+		}
+		
+		void operator()( std::string const& value ) {
+			(*m_result)[m_index++] = value ;
+		}
+	private:
+		Rcpp::StringVector* m_result ;
+		std::size_t m_index ;
+	} ;
 }
-#endif
+
+
 // [[Rcpp::export]]
 Rcpp::List giveMeMyData(
 	std::string const& filename,
@@ -112,12 +151,12 @@ Rcpp::List giveMeMyData(
 
 	List result ;
 	{
-		std::size_t const N = view->number_of_variants() ;
-		CharacterVector chromosomes( N ) ;
-		IntegerVector positions( N ) ;
-		CharacterVector rsids( N ) ;
-		CharacterVector first_alleles( N ) ;
-		CharacterVector alternate_alleles( N ) ;
+		std::size_t const NV = view->number_of_variants() ;
+		CharacterVector chromosomes( NV ) ;
+		IntegerVector positions( NV ) ;
+		CharacterVector rsids( NV ) ;
+		CharacterVector first_alleles( NV ) ;
+		CharacterVector alternate_alleles( NV ) ;
 	
 		std::string SNPID ;
 		std::string rsid ;
@@ -125,22 +164,34 @@ Rcpp::List giveMeMyData(
 		uint32_t position ;
 		std::vector< std::string > alleles ;
 	
-		Dimension ploidy_dimension = Dimension( N, view->number_of_samples() ) ;
+		Dimension ploidy_dimension = Dimension( NV, view->number_of_samples() ) ;
 		IntegerVector ploidy = IntegerVector( ploidy_dimension ) ;
-		Dimension data_dimension = Dimension( N, view->number_of_samples(), 3ul ) ;
+		Dimension data_dimension = Dimension( NV, view->number_of_samples(), 3ul ) ;
 		NumericVector data = NumericVector( data_dimension ) ;
-
-		for( std::size_t i = 0 ; i < N; ++i ) {
+		
+		StringVector sampleNames( view->number_of_samples() ) ;
+		view->get_sample_ids( set_sample_names( &sampleNames )) ;
+		
+#if DEBUG
+		std::cerr << "ploidy.size() == " << ploidy.size() << "!\n" ;
+		std::cerr << "data.size() == " << data.size() << "!\n" ;
+#endif
+		
+		for( std::size_t i = 0 ; i < NV; ++i ) {
 			DataFiller filler( &ploidy, ploidy_dimension, &data, data_dimension, i ) ;
 			view->read_variant( &SNPID, &rsid, &chromosome, &position, &alleles ) ;
-			//view->read_genotype_data_block( filler ) ;
-			view->ignore_genotype_data_block() ;
+			view->read_genotype_data_block( filler ) ;
+			//view->ignore_genotype_data_block() ;
 			rsids[i] = rsid ;
 			first_alleles[i] = alleles[0] ;
 			alternate_alleles[i] = alleles[1] ;
 			chromosomes[i] = chromosome ;
 			positions[i] = position ;
 		}
+
+		data[0] = 1 ;
+		data[1] = 2 ;
+		data[3] = 3 ;
 
 		// For this example we assume diploid samples and two alleles
 		DataFrame variants = DataFrame::create(
@@ -150,8 +201,21 @@ Rcpp::List giveMeMyData(
 			Named("allele0") = first_alleles,
 			Named("allele1") = alternate_alleles
 		) ;
+		variants.attr( "row.names" ) = rsids ;
+	
+		StringVector genotypeNames(3) ;
+		genotypeNames[0] = "0" ;
+		genotypeNames[1] = "1" ;
+		genotypeNames[2] = "2" ;
+		List dataNames = List(3) ;
+		dataNames[0] = rsids ;
+		dataNames[1] = sampleNames ;
+		dataNames[2] = genotypeNames ;
+	
+		data.attr( "dimnames" ) = dataNames ;
 	
 		result[ "variants" ] = variants ;
+		result[ "samples" ] = sampleNames ;
 		result[ "ploidy" ] = ploidy ;
 		result[ "data" ] = data ;
 	}
